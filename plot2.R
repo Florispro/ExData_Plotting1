d4aacca5d381696812e11c0032323fe3c18c446f
unknown <floris@Q04224.quintiq.nl>
2015-10-06 11:36:57 +0200

    Improved code readability

--- plot2.R
+++ plot2.R
@@ -7,7 +7,9 @@ data <- read.table('household_power_consumption.txt',
 
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 data <- subset(data, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
-data$DateTime <- as.POSIXct(paste(as.character(data$Date), data$Time), format="%Y-%m-%d %H:%M:%S")
+# Need a datetime column for time series plots
+data$DateTime <- as.POSIXct(paste(as.character(data$Date), data$Time), 
+                            format="%Y-%m-%d %H:%M:%S")
 
 # 2. Draw time series and save to PNG
 png(file = "plot2.png")
--- plot4.R
+++ plot4.R
@@ -7,7 +7,9 @@ data <- read.table('household_power_consumption.txt',
 
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 data <- subset(data, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
-data$DateTime <- as.POSIXct(paste(as.character(data$Date), data$Time), format="%Y-%m-%d %H:%M:%S")
+# Need a datetime column for time series plots
+data$DateTime <- as.POSIXct(paste(as.character(data$Date), data$Time), 
+                            format="%Y-%m-%d %H:%M:%S")
 
 # 2. Draw four plots in 2x2 grid and save to PNG
 png(file = "plot4.png")
@@ -15,9 +17,26 @@ png(file = "plot4.png")
 par(mfrow = c(2, 2))
 
 with(data, {
-  plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
-  plot(DateTime, Voltage, type = "l", xlab = "datetime", ylab = "voltage")
-  plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+  # Plot 1 - Time series
+  plot(DateTime, 
+       Global_active_power, 
+       type = "l", 
+       xlab = "", 
+       ylab = "Global Active Power")
+  
+  # Plot 2 - Time series
+  plot(DateTime, 
+       Voltage, 
+       type = "l", 
+       xlab = "datetime", 
+       ylab = "voltage")
+  
+  # Plot 3 - Three time series
+  plot(DateTime, 
+       Sub_metering_1, 
+       type = "l", 
+       xlab = "", 
+       ylab = "Energy sub metering")
   points(DateTime, Sub_metering_2, col = "red", type = "l")
   points(DateTime, Sub_metering_3, col = "blue", type = "l")
   legend("topright", 
@@ -26,7 +45,12 @@ with(data, {
          bty = "n",
          col = c("black", "red", "blue"), 
          legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime")
+  
+  # Plot 4 - Time series
+  plot(DateTime, 
+       Global_reactive_power, 
+       type = "l", 
+       xlab = "datetime")
 })
 
 dev.off()
